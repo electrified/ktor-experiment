8955a20b2c3398fbff075c99662ac53f4e20252d
Ed Brindley <ed@maidavale.org>
2024-11-19 20:59:36 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "org.maidavale"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.resources)
+    implementation(libs.ktor.server.swagger)
+    implementation(libs.ktor.server.metrics)
+    implementation(libs.ktor.server.pebble)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.h2)
+    implementation(libs.ktor.server.kafka)
+    implementation(libs.mongodb.driver.core)
+    implementation(libs.mongodb.driver.sync)
+    implementation(libs.bson)
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.ktor.server.csrf)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ src/main/kotlin/org/maidavale/Application.kt
@@ -0,0 +1,18 @@
+package org.maidavale
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureMonitoring()
+    configureTemplating()
+    configureAdministration()
+    configureSerialization()
+//    configureDatabases()
+    configureSecurity()
+    configureRouting()
+}
